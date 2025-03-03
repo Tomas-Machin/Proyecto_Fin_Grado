03fcc22507f61d70671a8a6db4e60e4fcd5e6cd7
tmesteve <tm.esteve@gmail.com>
2025-03-03 17:08:28 +0100

    separacion de juego y mesa completado

--- game.py
+++ game.py
@@ -1,47 +1,37 @@
 from deck import Deck
 from player import Player
-
-import json
-
-poker = {
-    "POSICIONES_POKER": {
-        "UTG": {},
-        "MP": {},
-        "HJ": {},
-        "CO": {},
-        "BU": {},
-        "SB": {},
-        "BB": {}
-    }
-}
-
-POSICIONES_POKER = list(poker["POSICIONES_POKER"].keys())
+from table import Table
 
 class PokerGame:
     def __init__(self, user_name, num_players, user_position):
+        self.table = Table(num_players)
         self.user_name = Player(user_name)
-        self.num_players = num_players
         self.user_position = user_position.upper()
-        self.rivals = [Player("Rival") for i in range(num_players - 1)]
+        self.rivals = [Player("Rival") for _ in range(num_players - 1)]
         self.deck = Deck()
         self.validate_user_position()
         self.assign_info_into_positions()
 
+    def print_rival_names(self):
+        for rival in self.rivals:
+            print(rival.name)  # Todos los rivales tendrán el nombre "Rival"
+
     def validate_user_position(self):
-        if self.user_position not in POSICIONES_POKER:
+        print(self.user_position)
+        if self.user_position not in self.table.positions:
             exit("Posición inválida.")
 
     def assign_info_into_positions(self):
-        for index, position in enumerate(POSICIONES_POKER):
+        for position in self.table.positions:
             if self.user_position == position:
-                poker["POSICIONES_POKER"][self.user_position]["nombre"] = self.user_name.name
+                self.table.poker["POSICIONES_POKER"][self.user_position]["nombre"] = self.user_name.name
             else:
-                poker["POSICIONES_POKER"][position]["nombre"] = "Rival"
+                self.table.poker["POSICIONES_POKER"][position]["nombre"] = "Rival"
 
     def start_game(self):
         self.deck.shuffle()
         self.deal_initial_cards()
-        #self.play_rounds()
+        # self.play_rounds()
 
     def deal_initial_cards(self):
         for player in [self.user_name] + self.rivals:
@@ -49,13 +39,11 @@ class PokerGame:
             print(player.hand)
 
     def game_information(self):
-        posiciones_poker_json = json.dumps(poker, indent=4)
-
         print("\nCartas del jugador usuario:")
         print(f"{self.user_name.name}: {self.user_name.hand}")
 
         print("\nPosiciones y fichas de los jugadores:")
-        print(posiciones_poker_json)
+        print(self.table.get_poker_info())
 
     """def play_rounds(self):
         # Aquí irán las rondas de apuestas y lógica adicional
