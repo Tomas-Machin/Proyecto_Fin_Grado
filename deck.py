43e6e7040a62a517a1841134c233141928086f3c
tmesteve <tm.esteve@gmail.com>
2025-03-03 18:39:42 +0100

    cambio de palos por comodidad - emojis complejos

--- deck.py
+++ deck.py
@@ -2,7 +2,7 @@ import random
 
 class Deck:
     def __init__(self):
-        self.cards = [f"{rank}{suit}" for rank in "23456789TJQKA" for suit in "♠♥♦♣"]
+        self.cards = [f"{rank}{suit}" for rank in "23456789TJQKA" for suit in "HSCD"]   # "♠♥♦♣"
 
     def shuffle(self):
         random.shuffle(self.cards)
