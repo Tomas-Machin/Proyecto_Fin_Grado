b25b1f00634fec1dbfa9a67e36e401767e762f7e
tmesteve <tm.esteve@gmail.com>
2025-03-03 16:43:43 +0100

    separacion de datos del juego y la mesa 1.0

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ def solicitar_informacion_jugador():
     user_name = input("Introduce tu nombre: ")
     num_players = int(input("Introduce el número de jugadores (2-6): "))
     user_position = input("Introduce tu posición en la mesa (UTG, MP, HJ, CO, BU, SB, BB): ")
-    # print(f"{user_name}, tu posicion es: {user_position} en una mesa de {num_players} jugadores.")
+    #blinds = input("Introduce las ciegas de la mesa: ")
+    #user_hand = input("Introduce tu mano con el formato {[número][palo] [número][palo]} siendo los palos H, S, C, D (Hearts, Spades, Cloves, Diamonds): ")
+    players_pockets = []
+    """for i in range(0, num_players):
+        print(i)
+        money = input("Introduce el dinero del jugador en la posicion: ")"""
+
     return user_name, num_players, user_position
 
 if __name__ == "__main__":
---
+++ table.py
@@ -0,0 +1,20 @@
+import json
+
+class Table:
+    def __init__(self, num_players):
+        self.num_players = num_players
+        self.positions = self.assign_positions()
+        self.poker = {"POSICIONES_POKER": {position: {} for position in self.positions}}
+
+    def assign_positions(self):
+        position_options = {
+            2: ["SB", "BB"],
+            3: ["BU", "SB", "BB"],
+            4: ["CO", "BU", "SB", "BB"],
+            5: ["HJ", "CO", "BU", "SB", "BB"],
+            6: ["MP", "HJ", "CO", "BU", "SB", "BB"]
+        }
+        return position_options.get(self.num_players, [])
+
+    def get_poker_info(self):
+        return json.dumps(self.poker, indent=4)
