8a7c19f52e472c8da67fddb5a547402618852ca0
Tomas Machin <tm.esteve@gmail.com>
2025-03-11 18:13:00 +0100

    comentario e ideas para la red bayesiana

--- table.py
+++ table.py
@@ -13,12 +13,12 @@ class Table:
 
     def assign_positions(self, num_players):
         position_options = {
-            2: ["SB", "BB"],
-            3: ["BU", "SB", "BB"],
-            4: ["CO", "BU", "SB", "BB"],
-            5: ["HJ", "CO", "BU", "SB", "BB"],
-            6: ["MP", "HJ", "CO", "BU", "SB", "BB"],
-            7: ["UTG", "MP", "HJ", "CO", "BU", "SB", "BB"]
+            2: ["SB", "BB"],                                # NO TENER EN CUENTA - es un 1v1
+            3: ["BU", "SB", "BB"],                          # division 1-2 (posicion temprana-tardia)
+            4: ["CO", "BU", "SB", "BB"],                    # division 1-1-2 / 2-2 (posicion temprana-media-tardia) (temprana-tardia)
+            5: ["HJ", "CO", "BU", "SB", "BB"],              # division 2-1-2 (posicion temprana-media-tardia)
+            6: ["MP", "HJ", "CO", "BU", "SB", "BB"],        # division 2-2-2 (posicion temprana-media-tardia)
+            7: ["UTG", "MP", "HJ", "CO", "BU", "SB", "BB"]  # division 2-3-2 (posicion temprana-media-tardia)
         }
         return position_options.get(num_players, [])
 
